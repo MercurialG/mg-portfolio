399b7f31eabe8464fca7b7c63514f2ff97b940a2
George Rykunov <43174725+MercurialG@users.noreply.github.com>
2022-11-19 09:48:25 -0500

    Update sanity.ts

--- sanity.ts
+++ sanity.ts
@@ -1,9 +1,16 @@
 import { createClient } from "next-sanity";
 import createImageUrlBuilder from "@sanity/image-url";
 
+// export const config = {
+//   dataset: process.env.NEXT_PUBLIC_SANITY_DATABASE || "production",
+//   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECTID!,
+//   apiVersion: "2021-10-21",
+//   useCdn: process.env.NODE_ENV === "production",
+// };
+
 export const config = {
   dataset: process.env.NEXT_PUBLIC_SANITY_DATABASE || "production",
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECTID!,
+  projectId: "gf5mc918",
   apiVersion: "2021-10-21",
   useCdn: process.env.NODE_ENV === "production",
 };
